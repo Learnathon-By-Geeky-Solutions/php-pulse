9feb34301b01aeee39aecae9e9d15a5f7e6ff724
mahbub14 <mahbubhasan14@gmail.com>
2025-02-08 00:01:32 +0600

    sidebar item make active depending on page

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -13,27 +13,6 @@
             <!-- Dashboard Section -->
             <li class="menu-header">Dashboard</li>
             <li class="dropdown active">
-<<<<<<< Updated upstream
-                <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
-                <ul class="dropdown-menu">
-                    <li class=active><a class="nav-link" href="index-0.html">General Dashboard</a></li>
-                    <li><a class="nav-link" href="index.html">Ecommerce Dashboard</a></li>
-                </ul>
-            </li>
-            <li class="menu-header">Starter</li>
-            <li class="dropdown">
-                <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Layout</span></a>
-                <ul class="dropdown-menu">
-                    <li><a class="nav-link" href="layout-default.html">Default Layout</a></li>
-                    <li><a class="nav-link" href="layout-transparent.html">Transparent Sidebar</a></li>
-                    <li><a class="nav-link" href="layout-top-navigation.html">Top Navigation</a></li>
-                </ul>
-            </li>
-            <li><a class="nav-link" href="blank.html"><i class="far fa-square"></i> <span>Blank Page</span></a></li>
-
-
-
-=======
                 <a href="{{ route('admin.dashboard') }}" class="nav-link">
                     <i class="fas fa-fire"></i>
                     <span>Dashboard</span>
@@ -70,7 +49,6 @@
                     </li>
                 </ul>
             </li>
->>>>>>> Stashed changes
         </ul>
     </aside>
 </div>
