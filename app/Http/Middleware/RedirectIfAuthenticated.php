e929f55486f975e7512b90d121c8cce3ee37146b
Md. Sabbih Sarker <Sabbihkyau@gmail.com>
2025-02-14 14:50:18 +0600

    Autenticte in middleware

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -21,7 +21,11 @@ class RedirectIfAuthenticated
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
+             
+                if ($request->is('login')) {
+                    return $next($request); 
+                }
+                return redirect()->route('dashboard');
             }
         }
 
