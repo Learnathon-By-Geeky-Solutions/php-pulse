843e69a93e476ec57f839f2378d3e4f14897873d
Md. Sabbih Sarker <Sabbihkyau@gmail.com>
2025-02-15 21:08:40 +0600

    Fixed untracked changes and cleaned working directory

--- storage/framework/views/e08d56c52ba798feeb74331ffc96d403.php
+++ storage/framework/views/e08d56c52ba798feeb74331ffc96d403.php
@@ -2,40 +2,20 @@
     <div class="container">
         <div class="brand_border">
             <div class="row brand_slider">
+                              
+                <?php $__currentLoopData = $brands; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $brand): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
                 <div class="col-xl-2">
                     <div class="wsus__brand_logo">
-                        <img src="images/brand_logo_1.jpg" alt="brand" class="img-fluid w-100">
+                        <img src="<?php echo e(asset($brand->logo)); ?>" alt="<?php echo e($brand->name); ?>" class="img-fluid w-100">
                     </div>
                 </div>
+                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                
                 <div class="col-xl-2">
-                    <div class="wsus__brand_logo">
-                        <img src="images/brand_logo_2.jpg" alt="brand" class="img-fluid w-100">
-                    </div>
+                   
                 </div>
-                <div class="col-xl-2">
-                    <div class="wsus__brand_logo">
-                        <img src="images/brand_logo_3.jpg" alt="brand" class="img-fluid w-100">
-                    </div>
-                </div>
-                <div class="col-xl-2">
-                    <div class="wsus__brand_logo">
-                        <img src="images/brand_logo_4.jpg" alt="brand" class="img-fluid w-100">
-                    </div>
-                </div>
-                <div class="col-xl-2">
-                    <div class="wsus__brand_logo">
-                        <img src="images/brand_logo_5.jpg" alt="brand" class="img-fluid w-100">
-                    </div>
-                </div>
-                <div class="col-xl-2">
-                    <div class="wsus__brand_logo">
-                        <img src="images/brand_logo_6.jpg" alt="brand" class="img-fluid w-100">
-                    </div>
-                </div>
-                <div class="col-xl-2">
-                    <div class="wsus__brand_logo">
-                        <img src="images/brand_logo_3.jpg" alt="brand" class="img-fluid w-100">
-                    </div>
+                
+                
                 </div>
             </div>
         </div>
