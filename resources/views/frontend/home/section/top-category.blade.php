eb9037c296c34bf51c356bfd972c73c9dc187d0a
Md. Sabbih Sarker <Sabbihkyau@gmail.com>
2025-03-03 22:53:55 +0600

    Updated checkoutt and homepage settings

--- resources/views/frontend/home/section/top-category.blade.php
+++ resources/views/frontend/home/section/top-category.blade.php
@@ -1,3 +1,8 @@
+@php
+    $popularCategories = json_decode($popularCategory->value, true);
+    // dd($popularCategories)
+@endphp
+
 <section id="wsus__monthly_top" class="wsus__monthly_top_2">
     <div class="container">
         <div class="row">
@@ -19,14 +24,13 @@
         <div class="row">
             <div class="col-xl-12">
                 <div class="wsus__section_header for_md">
-                    <h3>Top Categories Of The Month</h3>
+                    <h3>Popular Categories</h3>
                     <div class="monthly_top_filter">
-                        <button class=" active" data-filter="*">music</button>
+                        <button class=" active" data-filter="*">All</button>
+                        @foreach ($popularCategories as $popularCategory)
                         <button data-filter=".cloth">clothing</button>
-                        <button data-filter=".elec">Electronic</button>
-                        <button data-filter=".spk">Speakers</button>
-                        <button data-filter=".cam">Cameras</button>
-                        <button data-filter=".wat">Watches</button>
+                        @endforeach
+                       
                     </div>
                 </div>
             </div>
