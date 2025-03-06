f353d175c59d169b1ec4390a63592c2c72bda607
mahbub14 <mahbubhasan14@gmail.com>
2025-03-06 23:28:07 +0600

    working with order detail page

--- resources/views/frontend/dashboard/layouts/sidebar.blade.php
+++ resources/views/frontend/dashboard/layouts/sidebar.blade.php
@@ -9,7 +9,7 @@
       <a class="" href="{{ route('user.dashboard') }}">
           <i class="fas fa-tachometer"></i> Dashboard
       </a>
-  </li>      <li><a href="dsahboard_order.html"><i class="fas fa-list-ul"></i> Orders</a></li>
+  </li><li><a href="dsahboard_order.html"><i class="fas fa-list-ul"></i> Orders</a></li>
     <li><a href="dsahboard_download.html"><i class="far fa-cloud-download-alt"></i> Downloads</a></li>
     <li><a href="dsahboard_review.html"><i class="far fa-star"></i> Reviews</a></li>
     <li><a href="dsahboard_wishlist.html"><i class="far fa-heart"></i> Wishlist</a></li>
