b49ace42635ba307aa53532dd59c38a99d0672a4
Md. Sabbih Sarker <Sabbihkyau@gmail.com>
2025-03-22 15:05:18 +0600

    Homepagesetting resolved popularCategory

--- resources/views/frontend/home/section/top-category.blade.php
+++ resources/views/frontend/home/section/top-category.blade.php
@@ -1,13 +1,5 @@
 @php
-
     $popularCategories = isset($popularCategory) && $popularCategory ? json_decode($popularCategory->value, true) : [];
-
-    if ($popularCategory && isset($popularCategory->value)) {
-        $popularCategories = json_decode($popularCategory->value, true);
-    } else {
-        $popularCategories = []; // Set an empty array or handle the case accordingly
-    }
-
 @endphp
 
 <section id="wsus__monthly_top" class="wsus__monthly_top_2">
@@ -33,7 +25,6 @@
                 <div class="wsus__section_header for_md">
                     <h3>Popular Categories</h3>
                     <div class="monthly_top_filter">
-
                         @php $products = []; @endphp
                         @foreach ($popularCategories as $index => $categoryData)
                             @php
@@ -53,12 +44,6 @@
                                 <button class="{{ $loop->first ? 'auto_click active' : '' }}" data-filter=".category-{{$index}}">{{$category->name}}</button>
                             @endif
                         @endforeach
-
-                        <button class=" active" data-filter="*">All</button>
-                        @foreach ($popularCategories as $popularCategory)
-                        <button data-filter=".cloth">clothing</button>
-                        @endforeach
-
                     </div>
                 </div>
             </div>
